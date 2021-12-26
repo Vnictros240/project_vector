9c2cf0daf3f1597267824e02a4eeaad6c84e6c7b
Victor Sullivan <vssullivan@gmail.com>
2021-12-26 13:59:54 -0800

    Adding new page, bootstrap, extending base template

--- project_vector/routing.py
+++ project_vector/routing.py
@@ -2,10 +2,15 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-	return render_template("index.html", content="testing")
+@app.route("/<name>")
+def home(name):
+    return render_template("index.html")
+
+
+@app.route("/test")
+def home(name):
+    return render_template("new.html")
 
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
